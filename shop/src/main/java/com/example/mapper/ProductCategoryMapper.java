a7ecd3da846daf9b4db19957c38c7a788840512c
Dayeongs <ekdud9636@gmail.com>
2024-03-11 16:17:58 +0900

    실습(상품 - 카테고리, 상세조회, 순번표시), @ModelAttribute, <association /> 태그

---
+++ shop/src/main/java/com/example/mapper/ProductCategoryMapper.java
@@ -0,0 +1,15 @@
+package com.example.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.example.vo.ProductCategory;
+
+@Mapper
+public interface ProductCategoryMapper {
+
+	List<ProductCategory> getProductCategoies();
+	ProductCategory getProductCategoryByNo(int no);
+	
+}
